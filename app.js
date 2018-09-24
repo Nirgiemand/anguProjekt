88dc143325f87a87cde4a2f5cff7c81d52093a14
el Bobo <henri.tamvere@khk.ee>
2018-09-24 11:08:38 +0300

    Created basics and favicon, also connected AngularJS

---
+++ app.js
@@ -0,0 +1,11 @@
+var myApp = angular.module('myApp', []);
+
+myApp.controller('mainController', ['$scope', function ($scope) {
+
+    $scope.name = 'Main';
+    $scope.papa = 'Test works? Angular is not defined: but works... U wut M8, should I continue? Ofc.';
+}]);
+
+myApp.controller('secondController', ['$scope', function($scope) {
+    $scope.name = 'Õli';
+}]);
