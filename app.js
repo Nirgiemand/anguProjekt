4ea2f4562bbeca6641a24fe540f61d5e5a952a99
el Bobo <henri.tamvere@khk.ee>
2018-10-15 14:34:26 +0300

    Feedback form now works - sends emails to project master, and tried to add examples, but didn't work yet, needs some changes that will be modified in the next commit

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var myApp = angular.module('myApp', ['ngRoute']);
+var myApp = angular.module('myApp', ['ngRoute', 'ngAnimate']);
 
 myApp.config(function ($routeProvider) {
     $routeProvider
@@ -8,24 +8,23 @@ myApp.config(function ($routeProvider) {
             controller: 'mainController'
         })
 
-        .when('/second', {
+        .when('/todo', {
             controller: 'secondController',
             templateUrl: 'pages/todo.htm'
         })
-        .when('/third', {
+        .when('/example', {
             controller: 'thirdController',
             templateUrl: 'pages/example.htm'
-
         })
-        .when('/fourth', {
+        .when('/game', {
             templateUrl: 'pages/game.htm',
             controller: 'fourthController'
         })
-        .when('/fifth', {
+        .when('/feedback', {
             templateUrl: 'pages/feedback.htm',
             controller: 'fifthController'
         })
-        .when('/sixth', {
+        .when('/resource', {
             templateUrl: 'pages/resources.htm',
             controller: 'sixthController'
         })
@@ -78,73 +77,93 @@ myApp.controller('secondController', ['$scope', function ($scope) {
     };
 }]);
 myApp.controller('thirdController', ['$scope', function ($scope) {
-    
+
 }]);
 
-myApp.factory('game', function(){
-     var tileNames = ['8-ball', 'kronos', 'baked-potato', 'dinosaur', 'rocket', 'skinny-unicorn',
+myApp.factory('game', function () {
+    var tileNames = ['8-ball', 'kronos', 'baked-potato', 'dinosaur', 'rocket', 'skinny-unicorn',
     'that-guy', 'zeppelin'];
 
-  return new Game(tileNames);
+    return new Game(tileNames);
 });
 
-myApp.controller('fourthController', function($scope, game) {
+myApp.controller('fourthController', function ($scope, game) {
     $scope.game = game;
 });
 
-myApp.directive('mgCard', function() {
-  return {
-    restrict: 'E',
-    // instead of inlining the template string here, one could use templateUrl: 'mg-card.html'
-    // and then either create a mg-card.html file with the content or add
-    // <script type="text/ng-template" id="mg-card.html">.. template here.. </script> element to
-    // index.html
-    template: '<div class="container">' +
-                '<div class="card" ng-class="{flipped: tile.flipped}">' +
-                  '<img class="front" ng-src="img/Troll.png">' +
-                  '<img class="back" ng-src="img/{{tile.title}}.png">' +
-                '</div>' +
-              '</div>',
-    scope: {
-      tile: '='
+myApp.directive('mgCard', function () {
+    return {
+        restrict: 'E',
+        // instead of inlining the template string here, one could use templateUrl: 'mg-card.html'
+        // and then either create a mg-card.html file with the content or add
+        // <script type="text/ng-template" id="mg-card.html">.. template here.. </script> element to
+        // index.html
+        template: '<div class="container">' +
+            '<div class="card" ng-class="{flipped: tile.flipped}">' +
+            '<img class="front" ng-src="img/Troll.png">' +
+            '<img class="back" ng-src="img/{{tile.title}}.png">' +
+            '</div>' +
+            '</div>',
+        scope: {
+            tile: '='
+        }
     }
-  }
 });
 
 myApp.controller('fifthController', ['$scope', function ($scope) {
-    
+
 }]);
 
 myApp.controller('sixthController', ['$scope', function ($scope) {
     $scope.materjal = [{
-        "Name": "Routing",
-        "Description": 'I had couple errors with routing and couldn\'t find solution from online, so here\'s how you should mark href in your index.html file href="#!/".',
-        "Link": "https://www.w3schools.com/angular/angular_routing.asp"
+            "Name": "Routing",
+            "Description": 'I had couple errors with routing and couldn\'t find solution from online, so here\'s how you should mark href in your index.html file href="#!/".',
+            "Link": "https://www.w3schools.com/angular/angular_routing.asp"
     },
-    {
-        "Name": "W3Schools",
-        "Description": "Ofcourse, good old W3Schools, you can learn a lot from there.",
-        "Link": "https://www.w3schools.com/angular/default.asp"
+        {
+            "Name": "W3Schools",
+            "Description": "Ofcourse, good old W3Schools, you can learn a lot from there.",
+            "Link": "https://www.w3schools.com/angular/default.asp"
     },
-    {
-        "Name": "TutorialsPoint",
-        "Description": "Some people suggest this website for AngularJS learning, it seems fine and informative, but I did not use it.",
-        "Link": "https://www.tutorialspoint.com/angularjs/index.htm"
+        {
+            "Name": "TutorialsPoint",
+            "Description": "Some people suggest this website for AngularJS learning, it seems fine and informative, but I did not use it.",
+            "Link": "https://www.tutorialspoint.com/angularjs/index.htm"
     },
-    {
-        "Name": "CodeAcademy",
-        "Description": "Codeacademy offers a course for learning AngularJS.",
-        "Link": "https://www.codecademy.com/learn/learn-angularjs"
+        {
+            "Name": "CodeAcademy",
+            "Description": "Codeacademy offers a course for learning AngularJS.",
+            "Link": "https://www.codecademy.com/learn/learn-angularjs"
     }
 ];
     $scope.materjal1 = [{
-        "Name": "AngularJS Tutorial video",
-        "Description": "1-hour tutorial, I have watched many of these, but this one I found the most educative.",
-        "Link": "https://www.youtube.com/watch?v=0kmdjqgO9IY&t"
+            "Name": "AngularJS Tutorial video",
+            "Description": "1-hour tutorial, I have watched many of these, but this one I found the most educative.",
+            "Link": "https://www.youtube.com/watch?v=0kmdjqgO9IY&t"
     },
-    {
-        "Name": "Another tutorial video",
-        "Description": "Anything from Travesy Media is useful, you should really follow this channel!",
-        "Link": "https://www.youtube.com/watch?v=KhzGSHNhnbI&t=910s"
+        {
+            "Name": "Another tutorial video",
+            "Description": "Anything from Travesy Media is useful, you should really follow this channel!",
+            "Link": "https://www.youtube.com/watch?v=KhzGSHNhnbI&t=910s"
     }];
+}]);
+
+myApp.controller('firstController', ['$scope', '$route', function ($scope, $route) {
+    $scope.planet = {
+        name: 'Mercury',
+        carbondioxide: 'Trace Amount',
+        nitrogen: 'Trace Amount',
+        oxygen: '42%',
+        hydrogen: '22%'
+    };
+}]);
+
+myApp.controller('secundaController', ['$scope', '$route', function ($scope, $route) {
+    $scope.planet = {
+        name: 'Venus',
+        carbondioxide: '96.5%',
+        nitrogen: '3.5%',
+        oxygen: 'Almost None',
+        hydrogen: 'Almost None'
+    }
 }]);
--- pages/send_mail.php
+++ pages/send_mail.php
@@ -1,23 +1,25 @@
 <?php
 $webmaster_email = "henri.tamvere@khk.ee";
- /*
+/*
 This bit sets the URLs of the supporting pages.
 If you change the names of any of the pages, you will need to change the values here.
 */
 $feedback_page = "feedback.htm";
 $thankyou_page = "thank_you.htm";
- /*
+/*
 This next bit loads the form field data into variables.
 If you add a form field, you will need to add it here.
 */
-$email = $_REQUEST['email'] ;
-$message = $_REQUEST['message'] ;
-$name = $_REQUEST['name'] ;
+$email_address = $_REQUEST['email_address'] ;
+$comments = $_REQUEST['comments'] ;
+$first_name = $_REQUEST['first_name'] ;
+$last_name = $_REQUEST['last_name'];
 $msg = 
-"Name: " . $name . "\r\n" .  
-"Email: " . $email . "\r\n" . 
-"Comments: " . $message ;
- /*
+"First Name: " . $first_name . "\r\n" . 
+"Last Name: " . $last_name . "\r\n" . 
+"Email: " . $email_address . "\r\n" . 
+"Comments: " . $comments ;
+/*
 The following function checks for email injection.
 Specifically, it checks for carriage returns - typically used by spammers to inject a CC list.
 */
@@ -39,13 +41,24 @@ function isInjected($str) {
 		return false;
 	}
 }
- // If the user tries to access this script directly, redirect them to the feedback form,
+// If the user tries to access this script directly, redirect them to the feedback form,
 if (!isset($_REQUEST['email_address'])) {
 header( "Location: $feedback_page" );
 }
- // If we passed all previous tests, send the email then redirect to the thank you page.
-else {
- 	mail( "$webmaster_email", "Message from AngularJS page", $msg );
- 	header( "Location: $thankyou_page" );
+// If the form fields are empty, redirect to the error page.
+elseif (empty($first_name) || empty($email_address) || empty($last_name)) {
+header( "Location: $error_page" );
 }
-?> 
+/* 
+If email injection is detected, redirect to the error page.
+If you add a form field, you should add it here.
+*/
+elseif ( isInjected($email_address) || isInjected($first_name)  || isInjected($comments) || isInjected($last_name) ) {
+header( "Location: $error_page" );
+}
+// If we passed all previous tests, send the email then redirect to the thank you page.
+else {
+	mail( "$webmaster_email", "Letter from AngularJS", $msg );
+	header( "Location: $thankyou_page" );
+}
+?>
